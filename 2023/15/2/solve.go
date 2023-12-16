eee5e9f9c086ac86a0918680a38ca4e7797e1ec2
Harvey Lim <13660218+shzhe02@users.noreply.github.com>
2023-12-16 22:13:10 +0200

    day 15 done

--- 2023/15/2/solve.go
+++ 2023/15/2/solve.go
@@ -1,10 +1,79 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
+type Box struct {
+	labels []string
+	lenses map[string]int
+}
 
+// adds the lens or replaces the lens if it exists
+func (b *Box) Add(key string, val int) {
+	_, contained := b.lenses[key]
+	if !contained {
+		b.labels = append(b.labels, key)
+	}
+	b.lenses[key] = val
+}
+
+func (b *Box) Remove(key string) {
+	delete(b.lenses, key)
+	for idx, k := range b.labels {
+		if k == key {
+			b.labels = append(b.labels[:idx], b.labels[idx+1:]...)
+		}
+	}
+}
+
+func hash(in string) int {
+	total := 0
+	for _, c := range in {
+		total += int(c)
+		total *= 17
+		total %= 256
+	}
+	return total
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	var data []string
+	for scanner.Scan() {
+		in := scanner.Text()
+		if len(in) == 0 {
+			break
+		}
+		data = strings.Split(in, ",")
+	}
+	boxes := make(map[int]*Box)
+	// initializing boxes
+	for i := 0; i < 256; i++ {
+		boxes[i] = &Box{make([]string, 0), make(map[string]int)}
+	}
+	for _, part := range data {
+		if strings.HasSuffix(part, "-") {
+			currHash := hash(part[:len(part)-1])
+			boxes[currHash].Remove(part[:len(part)-1])
+		} else if strings.Contains(part, "=") {
+			kv := strings.Split(part, "=")
+			currHash := hash(kv[0])
+			val, _ := strconv.Atoi(kv[1])
+			boxes[currHash].Add(kv[0], val)
+		}
+	}
+	out := 0
+	for i, b := range boxes {
+		box := i + 1
+		for j, s := range b.labels {
+			slot := j + 1
+			out += box * slot * b.lenses[s]
+		}
+	}
+	fmt.Println(out)
 }
